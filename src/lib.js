ec452d11f55af79a1c1a6677c44581cd74699405
Sayali Jagtap <jagtapsayali0304@gmail.com>
2018-10-24 17:41:38 +0530

    Starter created for clojures

---
+++ src/lib.js
@@ -0,0 +1,16 @@
+const makeConstant = undefined;
+const makeCounterFromN = undefined;
+const makeCounterFromZero = undefined;
+const makeDeltaTracker = undefined;
+const makeFiboGenerator = undefined;
+const clone = undefined;
+const makeCycler = undefined;
+
+module.exports = {
+  makeConstant,
+  makeCounterFromZero,
+  makeCounterFromN,
+  makeDeltaTracker,
+  makeFiboGenerator,
+  makeCycler,
+};
---
+++ test/makeConstantTest.js
@@ -0,0 +1,36 @@
+const assert=require("assert");
+const makeConstant = require('../src/lib.js').makeConstant;
+
+const testMakeConstantWithNoArgs = function() {
+  let constantlyReturnTwo = makeConstant(2);
+  let constantlyReturnString = makeConstant('hello');
+  let constantlyReturnArray = makeConstant([1, 2, 3]);
+  let constantlyReturnObject = makeConstant({a: 1, b: 2});
+  assert.equal(2, constantlyReturnTwo());
+  assert.equal('hello', constantlyReturnString());
+  assert.deepEqual([1, 2, 3], constantlyReturnArray());
+  assert.deepEqual({a: 1, b: 2}, constantlyReturnObject());
+}
+
+const testMakeConstantWithSingleArg = function() {
+  let constantlyReturnTwo = makeConstant(2);
+  let constantlyReturnString = makeConstant('hello');
+  let constantlyReturnArray = makeConstant([1, 2, 3]);
+  let constantlyReturnObject = makeConstant({a: 1, b: 2});
+  assert.equal(2, constantlyReturnTwo(1));
+  assert.equal('hello', constantlyReturnString(1));
+  assert.deepEqual([1, 2, 3], constantlyReturnArray(1));
+  assert.deepEqual({a: 1, b: 2}, constantlyReturnObject(1));
+}
+const runTests = function() {
+  if(!makeConstant)
+    return;
+
+  testMakeConstantWithNoArgs();
+  console.log("testMakeConstantWithNoArgs passed");
+
+  testMakeConstantWithSingleArg();
+  console.log("testMakeConstantWithSingleArg passed");
+}
+
+runTests();
