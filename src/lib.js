9f7efa0f463f20253e0f4cd7ec0329c634000f69
Prince Verma <16836599+vprince001@users.noreply.github.com>
2018-10-26 09:58:41 +0530

    Code Refactor

--- src/lib.js
+++ src/lib.js
@@ -1,114 +1,68 @@
-const makeConstant = function(input) {
-  return constantlyReturnTwo  = function() {
-    return input;
-  }
+const makeConstant = function(value) {
+  return function() {
+    return value;
+  };
+};
 
-  return constantlyReturnString = function() {
-    return input;
-  }
-
-  return constantlyReturnArray = function() {
-    return input;
-  }
-
-  return constantlyReturnObject = function() {
-    return input;
-  }
-}
-
-const makeCounterFromN = function(input) {
-  let count = input;
-  return countApples = function() {
-    return count++;
-  }
-
-  return countMangoes = function() {
-    return count++;
-  }
-
-  return countOranges = function() {
-    return count++;
-  }
-
-  return countGrapes = function() {
-    return count++;
-  }
+const makeCounterFromN = function(countInitializer) {
+  let counter = countInitializer;
+  return function() {
+    return counter++;
+  };
 };
 
 const makeCounterFromZero = function() {
-  let count = 0;
-  return function() {
-    return count++;
-  }
-}
-
-const makeDeltaTracker = function(old) {
-  let deltaTracker = {old:0, delta:0, new:0};
-
-  return trackDelta = function(delta) {
-    deltaTracker.old = old;
-    deltaTracker.delta = delta;
-    if(delta == undefined) {
-      deltaTracker.delta = 0;
-    }
-
-    deltaTracker.new = deltaTracker.old + deltaTracker.delta;
-    old = deltaTracker.new;
-    let result = {};
-    return Object.assign(result, deltaTracker);
-  }
-}
-
-const makeFiboGenerator = function(input1, input2) {
-  let limit = 1;
-
-  return getNextFiboNumber = function() {
-    let numToMultiply = input1;
-    if(numToMultiply == undefined || input2 != undefined){
-      numToMultiply = 1;
-    }
-
-    let firstTerm = input1;
-    let secondTerm = input2;
-    if(input1 == undefined || input2 == undefined) {
-      firstTerm = 0;
-      secondTerm = 1;
-    }
-    for(let term = 1; term < limit; term++){
-      sum = firstTerm + secondTerm;
-      firstTerm = secondTerm;
-      secondTerm = sum;
-    }
-    limit++;
-    return firstTerm*numToMultiply;
-  }
+  return makeCounterFromN(0);
 };
 
-const makeCycler = function(inputArray) {
-  let index = 0;
-  let cycleArray = inputArray.slice("");
-  return function() {
-    indexToPrint = index;
-    index++;
-    if(index == cycleArray.length){
-      index = 0;
+const makeDeltaTracker = function(initialValue) {
+  let tracker = {old : initialValue, new : initialValue};
+  return function(deltaValue) {
+    if(!deltaValue){
+      deltaValue = 0;
     }
-    return cycleArray[indexToPrint];
-  }
-};
-
-const curry = function(operation, operand) {
-  return function(value1,value2) {
-    return operation(operand,value1,value2);
-  }
-};
-
-const compose = function(firstFunc, secondFunc){
-  return function(firstCollection, secondCollection) {
-    let output = secondFunc(firstCollection, secondCollection);
-    let result = firstFunc(output);
+    tracker.old = tracker.new;
+    tracker.delta = deltaValue;
+    tracker.new = tracker.delta + tracker.old;
+    let result = Object.assign({}, tracker);
     return result;
-  }
+  };
+};
+
+const makeFiboGenerator = function(second = 1, first = 0 ) {
+  let previous = Math.min(first,second);
+  let present = Math.max(first,second);
+  return function(){
+    let result = previous;
+    let next = previous + present;
+    previous = present;
+    present = next;
+    return result;
+  };
+};
+
+const makeCycler = function(collection) {
+  let currentIndex = 0;
+  let copyOfCollection = collection.slice("");
+  return function() {
+    let sizeOfCollection = copyOfCollection.length;
+    let indexToReturnValueOf = currentIndex % sizeOfCollection;
+    currentIndex++;
+    return copyOfCollection[indexToReturnValueOf];
+  };
+};
+
+const curry = function(combiner, initialValue) {
+  return function(valueToCombine, anotherValueToCombine) {
+    return combiner(initialValue, valueToCombine, anotherValueToCombine);
+  };
+};
+
+const compose = function(firstOperation, secondOperation){
+  return function(firstCollection, secondCollection) {
+    let output  = firstOperation(secondOperation(firstCollection, secondCollection));
+    return result;
+  };
 };
 
 exports.makeConstant=makeConstant;
